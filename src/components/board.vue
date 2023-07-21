d1a06690690178494a98fadf9e7726e0919b0a9c
Juli <yuliyamir19@gmail.com>
2023-07-21 21:36:24 +0300

    Lesson  26

--- src/components/board.vue
+++ src/components/board.vue
@@ -1,10 +1,66 @@
 <script>
+    import BoardColumn from './board-column.vue'
     export default {
         'name': "Board",
         data() {
             return {
-                hello: "Привет",
-                wallpaper: [
+              boards: [
+
+              ],
+          wallpaper: [
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/4.png",
+                "https://klevtsovaelena.github.io/wallpaper/img/5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire5.jpg",
                 "https://klevtsovaelena.github.io/wallpaper/img/Fire6.jpg",
                 "https://klevtsovaelena.github.io/wallpaper/img/Fire7.jpeg",
                 "https://klevtsovaelena.github.io/wallpaper/img/Fire8.png",
@@ -24,236 +80,150 @@
                 "https://klevtsovaelena.github.io/wallpaper/img/Flower10.jpg",
                 "https://klevtsovaelena.github.io/wallpaper/img/Flower11.jpg",
                 "https://klevtsovaelena.github.io/wallpaper/img/Flower12.jpg",
-                ],
-                currentWallpaper: 0,
-                activeMenu: false,
-                list: [
-                  {
-                    id: '1',
-                    title: 'Купить доски',
-                  },
-                  {
-                    id:'2',
-                    title: 'Нанять строителей',
-                  },
-                  {
-                    id: '3',
-                    title: 'Заплатить за аренду',
-                  },
-                ],
-
-                inprogress: [
-                  {
-                    id: '4',
-                    title: 'Закупить инструменты'
-                  },
-                ],
-
-                done: [
-                  {
-                    id: '5',
-                    title: 'Рассчитать денежные средства'
-                  },
-                ],
+              ],
+              currentWallpaper: 0,
+              activeMenu: false,
+              list: [
+                {
+                  id: '1',
+                  title: 'Купить доски'
+                },
+                {
+                  id: '2',
+                  title: 'Нанять строителей'
+                },
+                {
+                  id: '3',
+                  title: 'Заплатить за аренду'
+                },
+              ],
+              inprogress: [
+                {
+                  id: '4',
+                  title: 'Закупить инструменты'
+                },
+              ],
+              done: [
+                {
+                  id: '5',
+                  title: 'Рассчитать денежные средства'
+                },
+              ],
+              newTask: null,
             };
-        
-    } ,
-    props: ['test'],
-    methods: {
-      handlerDragend(event, type){
-       console.log('handlerDragend');
-       //console.log(event.target.getAttribute('id'));
-       //Получить элемент (колонку) на которую нужно переместить задачу
-       //elementsFromPoint - Функция из чистого JS, для получения его по координатам
-       let Element = document.elementsFromPoint(event.x, event.y)
-       console.log(element);
-       //Условие, при котором мы перехватываем, момент, 
-       //когда пользователь отпускает задачу в верхний белый блок с рукой
-       //Отпускаем элемент именно над блоком с классом drop-zone
-       if(Element.getAttribute('class')== 'drop-zone'){
-          console.log('Элемент опустился над drop-zone')
-          //Найти тот самый элемент, который я перетаскиваю
-          //1 - Из Ивента, получим идентификатор элемента
-          const id = event.target.getAttribute('id')
-          //2 - Найти элементы из конкретного массива
-          // 2.1 - Получить массив (list, inprogress или done) в переменную
-          let arrayFrom = this[type];
-          //2.2 - Найти в том массиве, задачу которую мы перетаскиваем
-          let element = arrayFrom.filter(el => el.id == id);
-          console.log('element', element)
-          // 2.3 - удалить элемент, из массива, из столбца которого мы перетаскиваем
-          //Получаем новый отфильтрованный массив, со всеми элементами, кромк того, что мы перетаскиваем
-          let filteredNewArray  = arrayFrom.filter(el => el.id !== id);
-          //Презаписываем массив из которого удалили элемент
-          this[type] = filteredNewArray
-          console.log('filteredNewArray', filteredNewArray)
-          //Получим имя массива, в который перемещаем задачу
-          let arrayTo  = Element\.getAttribute('name');
-          console.log('arrayTo', arrayTo)
-          //Получить массив в который мы хотим переместить элемент
-          unshift -элемент для добавления элемента в начало массива
-          this[arrayTo].unshift(element);
+        },
+        props: ['test'],
+        methods: { 
+          handlerDragend(event, type){
+            console.log('handlerDragend');
+            //console.log(event.target.getAttribute('id'));
+            //Получить элемент (колонку) на которую нужно переместить задачу
+            //elementFromPoint - Функция из чистого JS, для получения его по его координатам.
+            //https://developer.mozilla.org/en-US/docs/Web/API/Document/elementFromPoint
+            let Element = document.elementFromPoint(event.x, event.y);
+            console.log(Element);
+            //Условие, при котором мы перехватываеем, момент, 
+            //когда пользователь отпускает задачу в верхний белый блок с рукой
+            // Отпускаем элемент именно над блоком с классом drop-zone
+            if(Element.getAttribute('class') == 'drop-zone'){
+                console.log('Элемент опустился над drop-zone')
+                //Найти тот самый элемент, который я перетаскиваю
+                // 1 - Из Ивента, получим идентификатор элемента
+                const id = event.target.getAttribute('id')
+                // 2 - Найти элемент из конкретного массива
+                // 2.1 - получить массив (list, inprogress или done) в переменную из которого мы перетаскиваем задачу
+                let arrayFrom = this[type];
+                // 2.2 - Найти в том массиве, задачу которую мы перетаскиваем
+                let element = arrayFrom.find(el => el.id == id);
+                console.log('element',element)
+                // 2.3 - удалить элемент, из массива, из столбца которого мы перетаскиваем
+                //Получаем новый отфильтрованный массив, со всеми элементами, кроме того, что мы перетаскиваем
+                let filteredNewArray = arrayFrom.filter(el => el.id !== id);
+                //Перезаписываем массив из которого удалили элемент
+                this[type] = filteredNewArray
+                console.log('filteredNewArray',filteredNewArray)
+                //Получим имя массива, в который перемещаем задачи
+                let arrayTo = Element.getAttribute('name');
+                console.log('arrayTo', arrayTo)
+                //Получить массив в который переносим элемент и поместим элемент в его начало
+                //unshift - функция для добавление элемента в начало массива, противоположность функции push
+                this[arrayTo].unshift(element);
+            }
+          },
+          changeBackground(number){
+            this.currentWallpaper = number;
+            //Принудительно закрываем Меню
+            this.activeMenu = false;
+          },
+          openCloseMenu(){
+            //alert('Сработала функция openCloseMenu')
+            //Проверить открыто или закрыто сейчас Меню
+            if (this.activeMenu) {
+              //если перменная в значении true, то меняем на false
+              this.activeMenu = false;
+            } else {
+              //если перменная в значении false, то меняем на true
+              this.activeMenu = true;
+            }
+          },
+          addTask(task, columnName){
+            Готовим объект с задачей
+            const taskObject ={
+                id: Math.random(),
+                title: task
+            }
+            //Пушим в определенную колонку
+            this(columnMenu.push(task))
 
-          
-
-       }
-      },
-      changeBackground(number){
-        this.currentWallpaper=number;
-        //Принудительно закрыть Меню
-        this.activeMenu = false;
-      },
-      openCloseMenu(){
-        //alert('Сработала функция openCloseMenu')
-        //this.activeMenu = true;
-        //Проверить открыто или закрыто сейчас Меню
-        if  (this.activeMenu){
-        //Если переменная в значении true
-        this.activeMenu = false;
-        } else {
-          this.activeMenu = true;
-        }
+          }
+        },
+     // Регистрируем внешний компонент внутри родительского
+      components: {
+        'board-column': BoardColumn
       }
     }
-
-    }
-    
 </script>
 
 <template>
-<div>
-    
-    <div class='board' :style="{ 'background-image': `url(${wallpaper[currentWallpaper]})`}" >
+  <div>
+    <div class='board' :style="{'background-image': `url(${wallpaper[currentWallpaper]})` }"> 
+
       <button class='board__button-change-theme' @click='openCloseMenu'>
-        <img :src='wallpaper[currentWallpaper]' />Сменить фон
+         <img :src='wallpaper[currentWallpaper]' />
+         Сменить фон
       </button>
+    
 
-    <div :class="[activeMenu ? 'board__theme-wrapper board__theme-wrapper--active' : 'board__theme-wrapper']">
-       <div id='wallpapers'>
-          <div
-          v-for="(item, index) in wallpaper"
-          class='wallpapers__item'
-          :key="index"
-          :style="{ 'background-image': `url(${item})`}"
-          @click='changeBackground(index)'
-      >
-       </div>
+      <div :class="[activeMenu ? 'board__theme-wrapper board__theme-wrapper--active' : 'board__theme-wrapper']">
+          <div id='wallpapers'>
+            <div 
+              v-for="(item, index) in wallpaper"
+              class='wallpapers__item'
+              :key="index"
+              :style="{'background-image': `url(${item})` }"
+              @click='changeBackground(index)'
+            >
+            </div>
+          </div>
+          <div class='close-wallpapers' @click='openCloseMenu'>
+              {{'<'}}
+          </div>
+      </div>
+      <board-column :addTask="addTask" :taskModel="newTask" title="На складе" :items="list" name="list" :handlerDragend='handlerDragend'></board-column>
+      <board-column :addTask="addTask" :taskModel="newTask" title="У курьера" :items="inprogress" name="inprogress" :handlerDragend='handlerDragend'></board-column>
+      <board-column :addTask="addTask" :taskModel="newTask" title="Длставлено" :items="done" name="done" :handlerDragend='handlerDragend'></board-column>
+
+      
+      
+
+      
+
     </div>
-
-      <div class='close-wallpapers' @click='openCloseMenu'>
-      {{'<'}}
-      </div>
-  
-
-   
-    </div>
-
-    <section class='board-content'>
-      <h2>
-        На складе
-      </h2>
-      <input
-        placeholder='Новый товар'
-        type='text'
-        class='new-todo-item'
-      >
-      <div
-        class='drop-zone'
-        name='list'
-      >
-        <img src='public/assets/cursor.png'>
-      </div>
-      <ul class='todo-list board__column'>
-        <li
-          class='todo-item shoping__item'
-          v-for="(item, index) in list"
-          :key="item.id"
-          draggable="true"
-          @dragend="handlerDragend($event, 'list')"
-          :id="item.id
-        >
-
-      {{ item.title }}
-        
-        </li>
-      </ul>
-
-    </section>
-   
-
-    <section class='board-content'>
-      <h2>
-        У курьера
-      </h2>
-      <input
-        placeholder='Новый товар'
-        type='text'
-        class='new-todo-item'
-      >
-      <div
-        class='drop-zone'
-        name='inprogress'
-      >
-        <img src='public/assets/cursor.png'>
-      </div>
-      <ul class='todo-list board__column'>
-        <li
-          class='todo-item shoping__item'
-          v-for="(item, index) in inprogress"
-          :key="item.id"
-          draggable="true"
-          @dragend="handlerDragend($event, 'inprogress')"
-          :id="item.id
-        >
-        
-          {{ item.title}}
-          
-        </li>
-      </ul>
-
-    </section>
-
-    <section class='board-content'>
-      <h2>
-        Доставлено
-      </h2>
-      <input
-        placeholder='Новый товар'
-        type='text'
-        class='new-todo-item'
-      >
-      <div
-        class='drop-zone'
-        name='done'
-      >
-        <img src='public/assets/cursor.png'>
-      </div>
-      <ul class='todo-list board__column'>
-        <li
-          class='todo-item shoping__item'
-          v-for="(item, index) in done"
-          :key="item.id"
-          draggable="true"
-          @dragend="handlerDragend($event, 'done')"
-          :id="item.id
-        >
-          
-        >
-          {{ item.title}}
-        </li>
-      </ul>
-
-    </section>
-   
-   
-
-</div>
-</div>
-
+  </div>
 </template>
 
 <style scoped>
-    .close-wallpapers{
+  .close-wallpapers{
     display: flex;
     position: fixed;
     top:0;
@@ -265,7 +235,7 @@
     background-color: rgba(255, 255, 255, 0.9);
   }
   .board{
-      height: 100%;
+      height: 100%; 
       display: flex;
       justify-content: center;
       position: absolute;
@@ -322,57 +292,16 @@
     max-width: 50px;
     margin-right: 10px;
   }
-  .drop-zone{ 
-    background: white;
-      height: 150px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-  }
-  .drop-zone img{
-    max-width: 50px;
-    margin: 10px;
-  }
-  body {
-    font-family: "Source Sans Pro", "Arial", sans-serif;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  .board section{
-    width: 30%;
-    margin: 5px;
-  }
-  .todo-list {
-    list-style-type: none;
-    padding: 10px;
-  }
   .done {
     text-decoration: line-through;
     color: #888;
   }
-  .new-todo {
-    width: 100%;
-  }
-  .trash-drop {
-    border: 2px dashed #ccc !important;
-    text-align: center;
-    color: #e33;
-  }
+  
+  
   .trash-drop:-moz-drag-over {
     border: 2px solid red;
   }
-  .todo-item {
-    position: relative;
-    border: 1px solid #ccc;
-    border-radius: 2px;
-    padding: 14px 8px;
-    margin-bottom: 3px;
-    background-color: #fff;
-    box-shadow: 1px 2px 2px #ccc;
-    font-size: 22px;
-    color: black;
-  }
+ 
   .remove-item {
     float: right;
     color: #a45;
@@ -394,39 +323,8 @@
   .board {
     display: flex;
   }
-  .board__column{
-    background-color: #ededed;
-    text-align: center;
-    min-width: 100%;
-    min-height: 80vh;
-  }
-  .board__column--in-stock{
-    width: 30%;
-    min-width: 300px;
-  }
-  .shoping__img{
-    max-width: 50px;
-    margin-right: 20px;
-  }
-  ul{
-    list-style: none;
-  }
-  .shoping__item{
-    background-color: white;
-    color: black;
-    margin: 15px 0;
-    padding: 10px;
-    border-radius: 15px;
-  }
-  .shoping__item a{
-    display: flex;
-    align-items: center;
-    color: black;
-  }
-  .shoping__item:hover{
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  }
-  .shoping__title{
-    font-size: 24px;
-  }
+ 
+ 
+  
+ 
   </style>
